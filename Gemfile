3e1715df70b479792f4bd5da3fa0df8078b63846
Marcos Augusto <x@nofxx.com>
2010-01-30 22:09:20 -0200

    bundler

---
+++ Gemfile
@@ -0,0 +1,28 @@
+#
+# CodeSolo Gems
+#
+# clear_sources
+# source "http://gemcutter.org"
+source "http://gems.github.com"
+
+bundle_path "vendor/bundler_gems"
+
+gem "rails", "2.3.5"
+
+gem "haml"
+gem "json"
+gem "authlogic"
+gem "authlogic-oid", :require_as => "authlogic_openid"
+gem "ruby-openid", :require_as => "openid"
+gem "will_paginate"
+gem "paperclip"
+gem "octopi"
+gem "symbolize"
+gem "api_cache", :require_as => false
+gem "bcrypt-ruby", :require_as => "bcrypt"
+
+only :test do
+  gem "rspec"
+  gem "rspec-rails"
+end
+
--- config/environment.rb
+++ config/environment.rb
@@ -10,15 +10,15 @@ Rails::Initializer.run do |config|
   config.i18n.load_path += Dir[File.join(RAILS_ROOT, "lib", "locales", "*.{rb,yml}")]
   config.i18n.default_locale = "en"
 
-  config.gem "haml"
-  config.gem "authlogic"
-  config.gem "authlogic-oid", :lib => "authlogic_openid"
-  config.gem "ruby-openid", :lib => "openid"
-  config.gem "will_paginate"
-  config.gem "paperclip"
-  config.gem "octopi"
-  config.gem "api_cache", :lib => false
-  config.gem "nofxx-symbolize", :lib => "symbolize"
+#   config.gem "haml"
+#   config.gem "authlogic"
+#   config.gem "authlogic-oid", :lib => "authlogic_openid"
+#   config.gem "ruby-openid", :lib => "openid"
+#   config.gem "will_paginate"
+#   config.gem "paperclip"
+#   config.gem "octopi"
+#   config.gem "api_cache", :lib => false
+#   config.gem "nofxx-symbolize", :lib => "symbolize"
 
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
